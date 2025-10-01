b7f2fc2f3345bd6bd937a5b98ae3dcaa40b2a408
KevinStirling <youngks93@gmail.com>
2025-09-30 22:03:09 -0400

    testing alt layout

--- ui/components/schedule/schedule.go
+++ ui/components/schedule/schedule.go
@@ -95,26 +95,17 @@ func renderSchedule(g data.Schedule) string {
 			if game.Status == "Live" {
 				var rows [][]string
 				rows = [][]string{
-					{game.HomeAbbr, game.AwayAbbr, game.On2B, data.SetOut(game.Outs, 1), func() string {
-						if game.InningSt == "Top" {
-							return game.InningArrow
-						}
-						return ""
-					}()},
-					{strconv.Itoa(game.HomeRuns), strconv.Itoa(game.AwayRuns), game.On3B + " - " + game.On1B, data.SetOut(game.Outs, 2), strconv.Itoa(game.Inning)},
+					{game.HomeAbbr, game.AwayAbbr, game.On2B},
+					{strconv.Itoa(game.HomeRuns), strconv.Itoa(game.AwayRuns), game.On3B + " - " + game.On1B, fmt.Sprintf("%s", strconv.Itoa(game.Balls)+"-"+strconv.Itoa(game.Strikes))},
 					{renderBp(game, true), renderBp(game, false),
-						fmt.Sprintf("%s", strconv.Itoa(game.Balls)+"-"+strconv.Itoa(game.Strikes)), data.SetOut(game.Outs, 3),
-						func() string {
-							if game.InningSt == "Bottom" {
-								return game.InningArrow
-							}
-							return ""
-						}()},
+						game.InningArrow + " " + strconv.Itoa(game.Inning),
+						data.SetOut(game.Outs, 1) + " " + data.SetOut(game.Outs, 2) + " " + data.SetOut(game.Outs, 3),
+					},
 				}
 				var (
 					purple    = lipgloss.Color("99")
 					cellStyle = lipgloss.NewStyle().Padding(0, 1)
-					outsCol   = lipgloss.NewStyle().BorderLeft(false).Width(3).Align(lipgloss.Center)
+					// outsCol   = lipgloss.NewStyle().BorderLeft(false).Width(3).Align(lipgloss.Center)
 				)
 				t := table.New().
 					Width(SB_WIDTH).
@@ -126,9 +117,7 @@ func renderSchedule(g data.Schedule) string {
 						case 2:
 							return lipgloss.NewStyle().Width(9).Align(lipgloss.Center)
 						case 3:
-							return outsCol
-						case 4:
-							return cellStyle.Width(3)
+							return cellStyle.Width(9).Align(lipgloss.Center)
 						}
 						return cellStyle.Align(lipgloss.Center)
 					}).
