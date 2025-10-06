af29adda36ef6269487a12b88d920a3b60334545
KevinStirling <youngks93@gmail.com>
2025-10-05 21:10:16 -0400

    chore - space optimize batter / pitcher

--- ui/components/schedule/schedule.go
+++ ui/components/schedule/schedule.go
@@ -99,15 +99,15 @@ func renderBp(g data.Game, isHome bool) string {
 	switch side {
 	case "top":
 		if isHome {
-			return fmt.Sprintf("%s, P: %d", g.Pitcher, g.PitchCount)
+			return fmt.Sprintf("%s %dP", g.Pitcher, g.PitchCount)
 		} else {
-			return fmt.Sprintf("%s, %s", g.Batter, g.BatterAvg)
+			return fmt.Sprintf("%s %s", g.Batter, g.BatterAvg)
 		}
 	case "bottom":
 		if !isHome {
-			return fmt.Sprintf("%s, P: %d", g.Pitcher, g.PitchCount)
+			return fmt.Sprintf("%s %dP", g.Pitcher, g.PitchCount)
 		} else {
-			return fmt.Sprintf("%s, %s", g.Batter, g.BatterAvg)
+			return fmt.Sprintf("%s %s", g.Batter, g.BatterAvg)
 		}
 	default:
 		return ""
