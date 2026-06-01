88377ad67dfde57727b2c8c276d7376a3c3d51b7
David Dymko <dymkod@gmail.com>
2026-06-01 18:54:07 -0400

    bug: schedule fetch not showing games after midnight

--- ui/components/schedule/model.go
+++ ui/components/schedule/model.go
@@ -6,7 +6,10 @@ import (
 	"github.com/KevinStirling/scorebug.sh/internal/mlbstats"
 )
 
+// ScheduleClient is an interface for retrieving schedule and game data from mlbstats
 type ScheduleClient interface {
-	Schedule(date *time.Time) (mlbstats.Schedule, error)
+	// Schedule returns the schedule for the given date
+	Schedule(date time.Time) (mlbstats.Schedule, error)
+	// GameFeed returns the game feed for the given gameLink
 	GameFeed(gameLink string) (mlbstats.Feed, error)
 }
