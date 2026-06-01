65bf05b75aea5c8f3424f75562ec1a0ddd5fe2e4
David Dymko <dymkod@gmail.com>
2026-06-01 18:54:07 -0400

    bug: schedule fetch not showing games after midnight

---
+++ internal/mlbstats/client_test.go
@@ -0,0 +1,59 @@
+package mlbstats
+
+import (
+	"testing"
+	"time"
+)
+
+// dateOperand represents a date operand
+type dateOperand int
+
+// date operand constants
+const (
+	sameDay dateOperand = iota
+	dayPrior
+)
+
+func Test_determineTime(t *testing.T) {
+	type test struct {
+		currentDate  time.Time
+		expectedDate time.Time
+		comparison   dateOperand
+	}
+
+	year, month, day := time.Now().Date()
+	tt := map[string]test{
+		"current date - equal": {
+			currentDate:  time.Now(),
+			expectedDate: time.Now(),
+			comparison:   sameDay,
+		},
+		"current date - pre 5am": {
+			currentDate:  time.Date(year, month, day, 5, 0, 0, 0, time.Now().Location()),
+			expectedDate: time.Now().AddDate(0, 0, -1),
+			comparison:   dayPrior,
+		},
+		"current date - post 5am": {
+			currentDate:  time.Date(year, month, day, 6, 0, 0, 0, time.Now().Location()),
+			expectedDate: time.Now(),
+			comparison:   sameDay,
+		},
+	}
+
+	for k, v := range tt {
+		t.Run(k, func(t *testing.T) {
+			currentDate := determineTime(v.currentDate)
+
+			switch v.comparison {
+			case dayPrior:
+				if !currentDate.After(v.expectedDate) {
+					t.Errorf("expected %v to be less than %v", currentDate, v.expectedDate)
+				}
+			case sameDay:
+				if currentDate.Day() != v.expectedDate.Day() {
+					t.Errorf("expected %v to equal %v", currentDate, v.expectedDate)
+				}
+			}
+		})
+	}
+}
--- ui/components/schedule/schedule.go
+++ ui/components/schedule/schedule.go
@@ -17,7 +17,7 @@ import (
 type Model struct {
 	client     ScheduleClient
 	games      []data.ScoreBug
-	date       *time.Time
+	date       time.Time
 	Paginator  paginator.Model
 	tabs       []string
 	tabContent [3][]string
@@ -30,7 +30,7 @@ func NewModel(client ScheduleClient) Model {
 	now := time.Now()
 	d := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 
-	bugs := fetchScoreBugs(client, &d)
+	bugs := fetchScoreBugs(client, d)
 
 	p := paginator.New()
 	p.KeyMap = paginator.KeyMap{
@@ -47,7 +47,7 @@ func NewModel(client ScheduleClient) Model {
 		client:    client,
 		Paginator: p,
 		games:     bugs,
-		date:      &d,
+		date:      d,
 		tabs:      []string{"live", "scheduled", "final"},
 		ActiveTab: 0,
 	}
@@ -128,7 +128,7 @@ func (m Model) checkServer() tea.Cmd {
 	}
 }
 
-func fetchScoreBugs(client ScheduleClient, date *time.Time) []data.ScoreBug {
+func fetchScoreBugs(client ScheduleClient, date time.Time) []data.ScoreBug {
 	sched, err := client.Schedule(date)
 	if err != nil {
 		log.Fatal("failed to fetch schedule", "error", err)
